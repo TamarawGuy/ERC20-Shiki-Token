5c36da0b29682b3647053823896a8fe2a9242640
TamarawGuy <viktor.m.rumenov@gmail.com>
2023-07-10 19:16:52 +0300

    Smart contract of ERC20 ShikiToken

---
+++ contracts/ShikiToken.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.17;
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+
+contract ShikiToken is ERC20, ERC20Capped {
+    address payable public owner;
+    uint public blockReward;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this function.");
+        _;
+    }
+
+    constructor(
+        uint cap,
+        uint reward
+    ) ERC20("ShikiToken", "SHK") ERC20Capped(cap * (10 ** decimals())) {
+        owner = payable(msg.sender);
+        _mint(owner, 70000000 * (10 ** decimals()));
+        blockReward = reward * (10 ** decimals());
+    }
+
+    function _mint(
+        address account,
+        uint256 amount
+    ) internal override(ERC20Capped, ERC20) {
+        require(
+            ERC20.totalSupply() + amount <= cap(),
+            "ERC20Capped: cap exceeded"
+        );
+        super._mint(account, amount);
+    }
+
+    function _mintMinerReward() internal {
+        _mint(block.coinbase, blockReward);
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint value
+    ) internal virtual override {
+        if (
+            from == address(0) &&
+            to != block.coinbase &&
+            block.coinbase != address(0)
+        ) {
+            _mintMinerReward();
+        }
+        super._beforeTokenTransfer(from, to, value);
+    }
+
+    function setBlockReward(uint reward) public onlyOwner {
+        blockReward = reward * (10 ** decimals());
+    }
+
+    function destroy() public onlyOwner {
+        selfdestruct(owner);
+        
+    }
+}
